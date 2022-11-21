8f30ccd23c128caf1df1484606818dc970926ca2
Pranay Kothari <pranay.kothari109@gmail.com>
2022-11-21 21:46:39 +0530

    hashing password while creating and updating user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,8 +1,14 @@
 const users = require('../models/usersModel');
+const bcrypt = require('bcrypt');
 
 // module to create new user
 exports.createUser = async(req, res) => {
     try {
+        // hashing password
+        const salt = await bcrypt.genSalt(7);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+
+        // saving in db
         const response = await new users(req.body).save();
         res.send(response);
     } catch (err) {
@@ -15,6 +21,13 @@ exports.createUser = async(req, res) => {
 // module to update existing user
 exports.updateUser = async(req, res) => {
     try {
+        if(req.body.password) {
+            // hashing password
+            const salt = await bcrypt.genSalt(7);
+            req.body.password = await bcrypt.hash(req.body.password, salt);
+        }
+
+        // updating data in db
         const response = await users.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true });
         res.send(response);
     } catch (err) {
